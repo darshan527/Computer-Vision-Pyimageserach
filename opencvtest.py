f3edc6e2d8037d0d66ef14f80fb7c7f554359c7c
darshan527 <darshan.hosamani6@gmail.com>
2021-05-20 23:48:44 +0530

    test

--- opencvtest.py
+++ opencvtest.py
@@ -2,6 +2,7 @@
 import cv2
 import argparse
 import imutils
+import numpy as np
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", default="OpenCV_Logo.png", help="Path to image")
@@ -12,9 +13,15 @@ cv2.imshow("OpenCV", image)
 # cv2.waitKey(0)
 
 img2 = imutils.rotate(image, 40)
-cv2.imshow("Rotated", img2)
+# cv2.imshow("Rotated", img2)
 
 img3 = imutils.rotate_bound(image, 60)
-cv2.imshow("Rotate bounded", img3)
+# cv2.imshow("Rotate bounded", img3)
+
+# Color spaces
+
+nimg = np.ones(image.shape, dtype=np.uint8) * 100
+cv2.imshow("nimg", nimg)
+
 cv2.waitKey(0)
 print("End")
